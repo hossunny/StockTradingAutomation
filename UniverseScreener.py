4b9717c959d15d09f062c144230f52f552f2bfca
HosunChoi <smarlte27@gmail.com>
2021-01-25 18:15:55 +0900

    Annual & Quarter Quantile Comparison

--- UniverseScreener.py
+++ UniverseScreener.py
@@ -371,6 +371,181 @@ class UniverseScreener:
             total = pd.concat([total, df[[itm]]],axis=1)
         return total
 
+    def concatall(df_ls,dt_ls):
+        rst = pd.DataFrame()
+        for idx, df in enumerate(df_ls):
+            df[dt_ls[idx]+'-interval'] = df.index
+            df.reset_index(drop=True, inplace=True)
+            df.rename(columns={'mean':dt_ls[idx]+'-mean','gmean':dt_ls[idx]+'-gmean','label':dt_ls[idx]+'-label'},inplace=True)
+            rst = pd.concat([rst,df],axis=1)
+        return rst
+
+    def VisualTest_Y_v3(dt, conn, cut=10, term=22, itm='PBR', unit='Y', filterby=['PBR','PCR','POR']):
+        #ldr = Loader()
+        code_ls = list(pd.read_sql("select code from company_info",conn).code.values)
+        with open("./TradingDates.pickle", "rb") as fr:
+            td_days = pickle.load(fr)
+        """Finding start date"""
+        cn = conn.cursor()
+        cn.execute("select max(date) from daily_price where code='005930'")
+        last_update = cn.fetchone()[0].strftime("%Y-%m-%d")
+        start=''
+        end=''
+        date_ls=[]
+        if unit == 'Y':
+            next_year = str(int(dt[:4])+1)
+            for td in td_days:
+                if td >= next_year+'-01-01':
+                    if td <= next_year+'-03-03':
+                        start = td
+                    elif td <= str(int(next_year)+1)+'-03-03' and td <= last_update:
+                        end = td
+                    else :
+                        break
+            pointer = td_days.index(start)
+            while (pointer <= td_days.index(end)):
+                date_ls.append(td_days[pointer])
+                pointer += term
+        elif unit == 'Q':
+            print("Not implemented yet.")
+            pass
+        else :
+            raise ValueError("Can't be..")
+                
+        print("Start Date : ",start)
+        sub_ls = Filtering(dt, conn, by=filterby)
+        fn_df = ldr.GetFinance(dt[:4]+'-01-01', dt+'-31', item=itm, code_ls=sub_ls, unit='Y', colname='code')
+        fn_df.dropna(axis=1, inplace=True)
+        pr_df = ldr.GetPricePerTerm(date_ls, sub_ls, item='adjprice',colname='code')
+        pr_df.dropna(axis=1, inplace=True)
+        sub_ls = list(set(fn_df.columns).intersection(set(pr_df.columns)))
+        print("Universe Size : ",len(sub_ls))
+        fn_df = fn_df[sub_ls].T
+        fn_df.rename({dt:"value"},axis=1,inplace=True)
+        pr_df = pr_df[sub_ls]
+        pr_df = GetExpectedReturn(pr_df).T
+        pr_df.drop(columns=pr_df.columns[0],axis=1,inplace=True)
+        total = pd.concat([fn_df,pr_df],axis=1)
+        raw_df = total.copy()
+        total = total.groupby(pd.qcut(total['value'],cut)).agg(['mean'])
+        total.columns = ['value']+date_ls[1:]
+        date_ls = date_ls[1:]
+        tmp = pd.DataFrame(total.drop(['value'],axis=1).T.mean())
+        tmp.columns = list(tmp.columns[:-1])+['mean']
+        labels = [i+1 for i in range(len(total))]
+        total['label']=labels
+        total = pd.concat([total,tmp],axis=1)
+        #total.sort_values(by=['mean'],ascending=False,inplace=True)
+        tmp = total.drop(['value','label','mean'],axis=1) + 1
+        total['gmean'] = np.exp(np.log(tmp.prod(axis=1))/tmp.notna().sum(1)).values
+        total.sort_values(by=['gmean'],ascending=False,inplace=True)
+        
+        return total[['mean','gmean','label']]
+
+    def VisualTest_Q_v2(dt, conn, cut=10, term=5, itm='PBR', unit='1Q', filterby=['PBR','PCR','POR']):
+        #ldr = Loader()
+        code_ls = list(pd.read_sql("select code from company_info",conn).code.values)
+        with open("./TradingDates.pickle", "rb") as fr:
+            td_days = pickle.load(fr)
+        """Finding start date"""
+        cn = conn.cursor()
+        cn.execute("select max(date) from daily_price where code='005930'")
+        last_update = cn.fetchone()[0].strftime("%Y-%m-%d")
+        start=''
+        end=''
+        date_ls=[]
+        year = dt[:4]
+        if unit=='1Q':
+            for td in td_days:
+                if td >= year+'-05-15':
+                    if td <= year+'-05-31':
+                        start = td
+                    elif td <= year+'-08-10' and td<=last_update:
+                        end = td
+                    else :
+                        break
+            pointer = td_days.index(start)
+            while (pointer <= td_days.index(end)):
+                date_ls.append(td_days[pointer])
+                pointer += term
+        elif unit=='2Q':
+            for td in td_days:
+                if td >= year+'-08-15':
+                    if td <= year+'-08-31':
+                        start = td
+                    elif td <= year+'-11-10' and td <= last_update:
+                        end = td
+                    else :
+                        break
+            pointer = td_days.index(start)
+            while (pointer <= td_days.index(end)):
+                date_ls.append(td_days[pointer])
+                pointer += term
+        elif unit=='3Q':
+            for td in td_days:
+                if td >= year+'-11-15':
+                    if td <= year+'11-31':
+                        start = td
+                    elif td <= str(int(year)+1)+'-03-01' and td <= last_update:
+                        end = td
+                    else :
+                        break
+            pointer = td_days.index(start)
+            while (pointer <= td_days.index(end)):
+                date_ls.append(td_days[pointer])
+                pointer += term
+        else :
+            raise ValueError("Not proper quarter!")
+        print("Start Date : ",start)
+        sub_ls = Filtering(dt, conn, by=filterby)
+        if unit=='1Q':
+            fn_df = ldr.GetFinance(dt[:4]+'-02-01', dt+'-31',item=itm, code_ls=sub_ls, unit='Q', colname='code')
+        elif unit=='2Q':
+            fn_df = ldr.GetFinance(dt[:4]+'-05-01', dt+'-31',item=itm, code_ls=sub_ls, unit='Q', colname='code')
+        elif unit=='3Q':
+            fn_df = ldr.GetFinance(dt[:4]+'-08-01', dt+'-31',item=itm, code_ls=sub_ls, unit='Q', colname='code')
+        #fn_df = ldr.GetFinance(dt[:4]+'-01-01', dt+'-31', item=itm, code_ls=sub_ls, unit='Q', colname='code')
+        fn_df.dropna(axis=1, inplace=True)
+        #pr_df = ldr.GetPrice(Q1, Q5, sub_ls, item='adjprice',colname='code')
+        pr_df = ldr.GetPricePerTerm(date_ls, sub_ls, item='adjprice',colname='code')
+                
+        #pr_df = pr_df[pr_df.index.isin([Q1,Q2,Q3,Q4,Q5])]
+        pr_df.dropna(axis=1, inplace=True)
+        sub_ls = list(set(fn_df.columns).intersection(set(pr_df.columns)))
+        print("Universe Size : ",len(sub_ls))
+        fn_df = fn_df[sub_ls].T
+        fn_df.rename({dt:"value"},axis=1,inplace=True)
+        pr_df = pr_df[sub_ls]
+        pr_df = GetExpectedReturn(pr_df).T
+        pr_df.drop(columns=pr_df.columns[0],axis=1,inplace=True)
+        total = pd.concat([fn_df,pr_df],axis=1)
+        raw_df = total.copy()
+        total = total.groupby(pd.qcut(total['value'],cut)).agg(['mean'])
+        #print(total.columns)
+        #print(date_ls)
+        total.columns = ['value']+date_ls[1:]
+        
+        date_ls = date_ls[1:]
+        color=iter(cm.rainbow(np.linspace(0,1,cut)))
+        plt.figure(figsize=(15,10))
+        plt.title("Total Expected Return at some points with {}".format(itm))
+        plt.xlabel(f"{itm} with {cut} quantile")
+        plt.ylabel('Expected Return Rate')
+        for i in range(cut):
+            c = next(color)
+            plt.plot(total.columns[1:], total.iloc[i,1:], color=c, marker='o', linestyle='--', label='{}-with-{}quantile'.format(itm,i+1))
+        #plt.legend(loc='best')
+        plt.legend(loc='upper left')
+        plt.grid(True)           
+        plt.show()
+        tmp = pd.DataFrame(total.drop(['value'],axis=1).T.mean())
+        tmp.columns = list(tmp.columns[:-1])+['mean']
+        labels = [i+1 for i in range(len(total))]
+        total['label']=labels
+        total = pd.concat([total,tmp],axis=1)
+        total.sort_values(by=['mean'],ascending=False,inplace=True)
+        return total.iloc[:3,:]   #, sub_ls, raw_df
+
 if __name__ == '__main__':
     print("Starting UniverseScreener...")
     #uvscr = UniverseScreener()
