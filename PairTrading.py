0428e46ed17580b566c99361e442e8984cd897b9
HosunChoi <smarlte27@gmail.com>
2021-02-11 20:58:48 +0900

    KOSPI data & PT-logic

---
+++ PairTrading.py
@@ -0,0 +1,135 @@
+import logging, os, pickle
+import requests, glob
+from datetime import datetime
+from bs4 import BeautifulSoup
+from datetime import datetime
+import pandas as pd
+import time
+from datetime import date
+import urllib.request
+from selenium.webdriver import Chrome
+import json, re, sys, h5py
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+import datetime as dt
+import pymysql
+import matplotlib.pyplot as plt
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from pykrx import stock
+import warnings
+warnings.filterwarnings(action='ignore')
+import shutil
+from matplotlib.pyplot import cm
+import numpy as np
+import scipy.stats as stats
+from scipy import stats
+import pyautogui
+from statsmodels.tsa.stattools import coint, adfuller
+from sklearn.linear_model import LinearRegression
+import Loader
+ldr = Loader.Loader()
+conn = pymysql.connect(host='localhost',user='root',
+                                   password='tlqkfdk2',db='INVESTAR',charset='utf8')
+
+
+def Correlation_v2(log_pr, cutoff=0.05):
+    rst = pd.DataFrame(columns=['corr','A','B'])
+    codes = list(log_pr.columns)
+    for i in range(len(codes)):
+        for j in range(len(codes)):
+            if i<j:
+                corr, pvalue = stats.pearsonr(log_pr[codes[i]], log_pr[codes[j]])
+                if pvalue <= cutoff:
+                    tmp = pd.DataFrame(columns=['corr','A','B'])
+                    tmp.loc[0,'corr'] = corr
+                    tmp.loc[0,'A'] = codes[i]
+                    tmp.loc[0,'B'] = codes[j]
+                    rst = pd.concat([rst, tmp])
+    return rst.sort_values(by=['corr'],ascending=False).reset_index(drop=True)
+
+
+def PartialCorrelation_v2(df, x,y,kospi):
+    x = np.array(df[x]).reshape(-1,1)
+    y = np.array(df[y]).reshape(-1,1)
+    start = df.index[0]
+    end = df.index[-1]
+    ksp = kospi[(kospi.index>=start)&(kospi.index<=end)]
+    z = np.array(ksp['close']).reshape(-1,1)
+
+    # Remove Market Factor by KOSPI
+    reg1 = LinearRegression().fit(z, x)
+    x_res = x - reg1.predict(z)
+    reg2 = LinearRegression().fit(z, y)
+    y_res = y - reg2.predict(z)
+
+    corr, pvalue = stats.pearsonr(x_res.reshape(-1,), y_res.reshape(-1,))
+    
+    return corr, pvalue
+
+
+
+def PairTrading(pr, start, end, ksp,cutoff=0.05):
+    """ 0) Log PR """
+    pr = pr.astype(float)
+    pr = pr[(pr.index>=start)&(pr.index<=end)]
+    pr = pr.dropna(axis=1,how='any')
+    log_pr = np.log(pr)
+    
+    """ 1) Correlation """
+    cor_rst = Correlation_v2(log_pr, cutoff)
+    print("Validation w.r.t Correlation : {}".format(len(cor_rst)))
+    
+    """ 2) Partial Correlation """
+    #ksp 임시로..!##################
+    #ksp = pd.read_csv("./FullCache/KOSPI_close.csv")
+    #ksp = ksp[(ksp.index>=start)&(ksp.index<=end)]
+    pcor_rst = pd.DataFrame(columns = ['A','B','corr','pcorr'])
+    for idx in range(len(cor_rst)):
+        a = cor_rst.loc[idx, 'A']
+        b = cor_rst.loc[idx, 'B']
+        pcor, ppvalue = PartialCorrelation_v2(log_pr, a, b, ksp)
+        tmp = pd.DataFrame(columns = ['A','B','corr','pcorr'])
+        if ppvalue <= cutoff :
+            tmp.loc[0,'A'] = a
+            tmp.loc[0,'B'] = b
+            tmp.loc[0,'corr'] = cor_rst.loc[idx,'corr']
+            tmp.loc[0,'pcorr'] = pcor
+            pcor_rst = pd.concat([pcor_rst, tmp])
+    print("Validation w.r.t Partial-Correlation by KOSPI : {}".format(len(pcor_rst)))
+    
+    #위 코드도 밑에처럼 loc추가로 바꾸자
+    
+    """ 3) CoIntegration """
+    # Curious about negative cointeg coeff
+    cointeg_rst = pd.DataFrame(columns = ['A','B','corr','pcorr','cointeg'])
+    for idx in range(len(pcor_rst)):
+        a = pcor_rst.loc[idx,'A']
+        b = pcor_rst.loc[idx,'B']
+        tmp_coeff, tmp_pvalue = coint(log_pr[a], log_pr[b])
+        if tmp_pvalue <= cutoff :
+            min_pvalue = tmp_pvalue
+            best_coeff = tmp_coeff
+        else :
+            min_pvalue = 1.0
+            best_coeff = -999.9
+        
+        for eta in [0.1*i for i in range(1,41)]:
+            spread = a - b * eta
+            adfuller_rst = adfuller(spread)
+            if adfuller_rst[1] <= cutoff:
+                if adfuller_rst[1] < min_pvalue:
+                    min_pvalue = adfuller_rst[1]
+                    best_coeff = eta
+                else :
+                    pass
+        if best_coeff != -999.9:
+            cointeg_rst.loc[idx,'A'] = a
+            cointeg_rst.loc[idx,'B'] = b
+            cointeg_rst.loc[idx,'corr'] = pcor_rst.loc[idx,'corr']
+            cointeg_rst.loc[idx,'pcorr'] = pcor_rst.loc[idx,'pcorr']
+            cointeg_rst.loc[idx,'cointeg'] = best_coeff
+    cointeg_rst.reset_index(drop=True, inplace=True)
+    
+    return cointeg_rst
